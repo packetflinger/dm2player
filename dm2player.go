d982a9a1b8b57b17c2e74d5eb4e852ab8e51a29a
Joe Reid <joe@joereid.com>
2021-09-28 14:03:53 -0400

    Command line args were wrong, working now. Moved redundant error checking to function

--- dm2player.go
+++ dm2player.go
@@ -15,55 +15,46 @@ type Config struct {
 
 func main() {
 	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
+	iferr(err)
 
 	sep := string(os.PathSeparator)
 	configfile := fmt.Sprintf("%s%sdm2player.json", user.HomeDir, sep)
 
 	configbody, err := os.ReadFile(configfile)
-	if err != nil {
-		panic(err)
-	}
+	iferr(err)
 
 	var config Config
 	err = json.Unmarshal(configbody, &config)
-	if err != nil {
-		panic(err)
-	}
+	iferr(err)
 
 	// copy the demo the right place
 	demoname := fmt.Sprintf("%s%s%s%stempdemo.dm2", config.Baseq2, sep, "demos", sep)
 	demosrc, err := os.ReadFile(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
+	iferr(err)
 
 	err = os.WriteFile(demoname, demosrc, 0666)
-	if err != nil {
-		panic(err)
-	}
+	iferr(err)
 
 	// make a temporary config and write it to baseq2 folder
-	cfg := "alias loopdemo \"disconnect; demo tempdemo; set nextserver loopdemo\""
+	cfg := "alias loopdemo \"disconnect; demo tempdemo; set nextserver loopdemo\"; loopdemo"
 	cfgname := fmt.Sprintf("%s%stempdemo.cfg", config.Baseq2, sep)
 	err = os.WriteFile(cfgname, []byte(cfg), 0666)
-	if err != nil {
-		panic(err)
-	}
+	iferr(err)
 
-	//cmd := fmt.Sprintf("%s +exec tempdemo.cfg", config.Q2exe)
-	cmd := exec.Command(config.Q2exe, "+demo tempdemo")
+	// spawn a quake 2 process to start playing the demo
+	cmd := exec.Command(config.Q2exe, "+exec", "tempdemo.cfg")
 	_ = cmd.Run()
 
+	// remove
 	err = os.Remove(demoname)
-	if err != nil {
-		panic(err)
-	}
+	iferr(err)
 
 	err = os.Remove(cfgname)
-	if err != nil {
-		panic(err)
+	iferr(err)
+}
+
+func iferr(e error) {
+	if e != nil {
+		panic(e)
 	}
 }
